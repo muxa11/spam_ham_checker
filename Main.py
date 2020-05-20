ab155b7844d84b079cf1a61d63307d950bfcf4b6
Musadiq <peermusadiq111@gmail.com>
2020-05-20 17:56:11 +0530

    Added Some imports

---
+++ Main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
